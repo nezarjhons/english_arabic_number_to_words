6d1fe332ea3698b70602d2789e0eb49d714b427b
nezar <nezar@nezar-PC>
2021-05-10 04:12:18 +0200

    english and arabic number to words converter

---
+++ src/NewMain.java
@@ -0,0 +1,23 @@
+/*
+ *  License for eControl for business solutions 2021
+ *  Created Date 10.05.2021
+ *  Created Time 04.09.15 AM
+ */
+
+/**
+ *
+ * @author nezar
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(TafqeetMain.convertNumberToEnglishWords("123456"));
+
+        System.out.println(TafqeetMain.convertNumberToArabicWords("123412.11"));
+        System.out.println(TafqeetMain.convertNumberToArabicWords("1122"));
+    }
+
+}
